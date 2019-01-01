d375be9a01309aca9e1a362b779ed30b22757172
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2019-01-01 12:12:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1691,7 +1691,7 @@ client.on('message', message => {
 var prefix = "*";
  
     if (message.author.id === client.user.id) return;
-    if (message.guild) {/Toxic Codes
+    if (message.guild) {
    let embed = new Discord.RichEmbed()
     let args = message.content.split(' ').slice(1).join(' ');
 if(message.content.split(' ')[0] == prefix + 'bc+') {/Toxic Codes
