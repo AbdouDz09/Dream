f79359f5380a4b6d33e9d1bcabf1af59a3b2dc22
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2019-01-01 11:28:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1687,6 +1687,32 @@ client.on('ready', function(){ //Toxic Codes
 
 });
 
+client.on('message', message => {
+var prefix = "*";
+ 
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {/Toxic Codes
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc+') {/Toxic Codes
+    if (!args[1]) {
+message.channel.send("**bc+ <message>**");
+return;
+}
+        message.guild.members.forEach(m => {/Toxic Codes
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField('» السيرفر :', `${message.guild.name}`)/Toxic Codes
+            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {/Toxic Codes
+        return;
+    }
 
 
 
