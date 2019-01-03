c83204994ba57fd3b3f21a8e07731624311c0a62
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2019-01-03 07:32:13 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1692,47 +1692,6 @@ client.on('ready', () => {
 });
 
 
-const suck = JSON.parse(fs.readFileSync('./suck.json', 'utf8'));
-
-
-
-
-client.on("message", message => {
-    fs.writeFile('./suck.json', JSON.stringify(suck));
-});
-// Toxic Codes
-client.on('ready', () => {
-    setInterval(function(){
-        client.guilds.forEach(g => {
-            if (suck[g.id]) {
-                if (suck[g.id].role) {// Toxic Codes
-                    var role = g.roles.get(suck[g.id].role);
-                    if (role) {
-                        role.edit({color : "RANDOM"});
-                    };// Toxic Codes
-                };// Toxic Codes 
-            };// Toxic Codes
-        });
-    }, 4000);// Toxic Codes
-})
-
-client.on("message", message => {
-    if (!message.content.startsWith(prefix)) return;
-    if (message.author.bot) return;
-    if (message.channel.type !== "text") return message.reply("This Command Is Only Allowed In Servers");
-    var args = message.content.split(" ");
-    var command = args[0].slice(prefix.length);// Toxic Codes
-    switch(command) {
-        case "set" :
-        if(!message.member.hasPermission('ADMINSTRATOR')) return message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINSTRATOR`' );
-        message.guild.createRole({name : "Drr .", color : "RANDOM"}).then(r => {// Toxic Codes
-            r.edit({color : "RANDOM"});
-            suck[message.guild.id] = {role : r.id};// Toxic Codes
-        });// Toxic Codes
-    };// Toxic Codes
-});// Toxic Codes
-
-
 
 
 
