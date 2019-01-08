8e6823e40e1e1a7d21e4790a12ea18c39c39985d
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2019-01-08 21:36:05 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1651,22 +1651,6 @@ client.user.setAvatar(argresult);
 	}
 });
 
-client.on('ready', function(){ //Toxic Codes
-    var ms = 30000 ;//Toxic Codes
-    var setGame = [`*help Dream Server `,` Dream By : Abdou_Dz`];//Toxic Codes
-    var i = -1;//Toxic Codes
-    var j = 0;//Toxic Codes
-    setInterval(function (){//Toxic Codes
-        if( i == -1 ){//Toxic Codes
-            j = 1;//Toxic Codes
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }//Toxic Codes
-        i = i+j;//Toxic Codes
-        client.user.setGame(setGame[i],`http://www.twitch.tv/abdoufersaoui_dz`);//Toxic Codes
-    }, ms);30000
-});
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag} !`);
