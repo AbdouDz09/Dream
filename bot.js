5c11de7b62f25d174d782a9ea387a32caadcfb37
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2019-01-08 21:50:25 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1635,6 +1635,22 @@ const devs = ['461468630773661699','403911754590978049'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
+	
+     if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+     if (message.content === (adminprefix + "leave")) {//Toxic Codes
+    message.guild.leave();   //Toxic Codes
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});//Toxic Codes
+      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
+  } else
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
+  } else
      
   if (message.content.startsWith(adminprefix + 'setname')) {
 client.user.setUsername(argresult).then
